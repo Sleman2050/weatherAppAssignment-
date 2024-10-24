c69f999c571f70c62a0fc79c4861a131a3903067
Sleman2050 <94982813+Sleman2050@users.noreply.github.com>
2024-10-24 22:41:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,14 @@ function searchWithXHR() {
             
             highTemp.innerText = `High: ${responseObj.current.feelslike_c}C`;
             lowTemp.innerText = `Low: ${responseObj.current.temp_c}C`;
-            wind.innerText = `Wind Speed: ${responseObj.current.wind_kph} k/h`;
-
+            wind.innerText = `Wind Speed: ${responseObj.current.wind_kph} kph`;
         }
     };
     xhr.open("GET", `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`, true);
     xhr.send();
 }
 
-//  Fetch 
+// Fetch 
 function searchWithFetch() {
     var city = document.getElementById("input").value;
 
@@ -39,33 +38,36 @@ function searchWithFetch() {
             return response.json();
         })
         .then(data => {
-            highTemp.innerText = `High: ${responseObj.current.feelslike_c}C`;
-            lowTemp.innerText = `Low: ${responseObj.current.temp_c}C`;
-            wind.innerText = `Wind Speed: ${data.current.wind_kph} kph`;
-
+            highTemp.innerText = `High: ${data.current.feelslike_c}C`; // Use data here
+            lowTemp.innerText = `Low: ${data.current.temp_c}C`; // Use data here
+            wind.innerText = `Wind Speed: ${data.current.wind_kph} kph`; // Use data here
         })
-
+        
 }
 
-// Fetch and (Async,Await)
+// Fetch and (Async/Await)
 async function searchWithFetchAwAs() {
     var city = document.getElementById("input").value;
+
     var highTemp = document.getElementById("high");
     var lowTemp = document.getElementById("low");
     var wind = document.getElementById("wind");
 
+   
+        let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
 
-    let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
-
-    let data = await response.json();
-    highTemp.innerText = `High: ${responseObj.current.feelslike_c}C`;
-    lowTemp.innerText = `Low: ${responseObj.current.temp_c}C`;
-    wind.innerText = `Wind Speed: ${data.current.wind_kph} kph`;
-
+        let data = await response.json();
+        highTemp.innerText = `High: ${data.current.feelslike_c}C`; // Use data here
+        lowTemp.innerText = `Low: ${data.current.temp_c}C`; // Use data here
+        wind.innerText = `Wind Speed: ${data.current.wind_kph} kph`; // Use data here
+    
 }
-
 
 // listeners
 document.getElementById("getWeatherXHR").addEventListener("click", searchWithXHR);
 document.getElementById("getWeatherFetch").addEventListener("click", searchWithFetch);
 document.getElementById("getWeatherAsyncAwait").addEventListener("click", searchWithFetchAwAs);
+
